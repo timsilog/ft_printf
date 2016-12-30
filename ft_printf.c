e74e9399e7e22015d437b3304c751ab4f185d318
Tim Jose <timjose3@gmail.com>
2016-12-29 19:49:08 -0800

    quick update

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,48 @@
 /*   By: tjose <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/27 17:27:31 by tjose             #+#    #+#             */
-/*   Updated: 2016/12/27 17:57:07 by tjose            ###   ########.fr       */
+/*   Updated: 2016/12/29 19:48:48 by tjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *format, ...)
+void	handle_flags(char c, int flag[5])
 {
-	va_list	flags;
+	int i;
+
+	i = -1;
+	while (++i < 5)
+		flag[i] = 0;
+	if (c == '-')
+		flag[0]++; // left justify (uses width)
+	if (c == '+')
+		flag[1]++; // show sign
+	if (c == ' ')
+		flag[2]++; // show space if no sign
+	if (c == '#')
+		flag[3]++; /* precede o, x, X with 0, 0x, 0X
+					  force e, E, f, F to contain decimal */
+	if (c == '0')
+		flag[4]++; // fills leftover width with zeroes
+}
+
+int		ft_printf(const char *format, ...)
+{
+	va_list	tags;
 	int		i;
+	int		flag[5];
 	
 	if (!format) // format[0]?
 		return (0);
-	va_start(flags, format);
+	va_start(tags, format);
 	i = -1;
 	while (format[++i])
 	{
 		if (format[i] == '%')
 		{
 		// handle flags
+			handle_flags(format[i], flag);
 	
 		// handle width
 	
@@ -34,6 +56,7 @@ int ft_printf(const char *format, ...)
 		// handle length
 	
 		// handle specifier
+			handle_specifer(format[i], flags);
 		}
 		else
 			write(1, &format[i], 1);
--- handle_specifier.c
+++ handle_specifier.c
@@ -1,35 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   handle_specifier.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tjose <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/27 15:59:32 by tjose             #+#    #+#             */
-/*   Updated: 2016/12/29 19:48:51 by tjose            ###   ########.fr       */
+/*   Created: 2016/12/29 17:43:16 by tjose             #+#    #+#             */
+/*   Updated: 2016/12/29 19:48:50 by tjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdarg.h>
+#include "ft_printf.h"
 
-int fun(int cnt, ...)
+void	handle_specifier(char c, va_list tags)
 {
-	int sum = 0;
-	va_list lst;
-	va_start(lst, cnt);
-	int i = 0;
-	while (i < cnt)
-	{
-		int n = va_arg(lst, int);
-		sum += n;
-		i++;
-	}
-	return (sum);
-}
-
-int main()
-{
-	//printf("%d\n%d", fun(4,1,2,3,4), fun(3,2,5,8));
-	printf("%-.23e", (double)-1234452345601);
+	if (c == 'd' || c == 'i')
+		handle_decimal(flags);
+	/*
+	if (c == 'D')
+	if (c == 'c')
+	if (c == 'C')
+	if (c == 's')
+	if (c == 'S')
+	if (c == 'p')
+	if (c == 'o')
+	if (c == 'O')
+	if (c == 'u')
+	if (c == 'U')
+	if (c == 'x')
+	if (c == 'X')
+	*/
 }
