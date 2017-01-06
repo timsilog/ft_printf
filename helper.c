de820f2ed0501f7a76408311b965489c16dead93
Tim Jose <timjose3@gmail.com>
2017-01-06 15:10:24 -0800

    moved stuff around

--- helper.c
+++
@@ -1,9 +0,0 @@
-
-#include "ft_printf.h"
-
-int		is_num(char c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
--- srcs/handle_width.c
+++ srcs/handle_width.c
@@ -11,7 +11,7 @@ int		handle_width(const char *str, t_mods *mods, va_list tags)
 		mods->width = va_arg(tags, int);
 		return (1);
 	}
-	while (is_num(str[i]))
+	while (ft_isdigit(str[i]))
 		mods->width = mods->width * 10 + (str[i++] - '0');
 	return (i);
 }
